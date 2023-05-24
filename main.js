0b1860be16df3dabb3ec5f7cdba2fa0e3c37c1f1
platara <plat0033@algonquinlive.com>
2023-05-24 13:58:59 -0400

    add starter files

---
+++ main.js
@@ -0,0 +1,83 @@
+//Arrow Functions
+//Basic  syntax
+const functionName = (parameters) => {
+    // function body
+    return value;
+  };
+// using one parameter, and a single expression to return
+const funcName = parameter => value;
+  
+//double a number
+
+
+
+
+
+
+
+
+
+// Functional Methods
+// forEach()
+// Array of strings
+const words = ["apple", "banana", "orange", "kiwi", "grape"];
+
+
+// Maps()
+// Array of temperatures in Celsius
+const celsiusTemperatures = [20, 25, 30, 35, 40];
+
+
+
+
+// Filter()
+
+// Array of products
+const products = [
+  { name: "T-shirt", price: 25 },
+  { name: "Jeans", price: 80 },
+  { name: "Sneakers", price: 60 },
+  { name: "Hat", price: 15 },
+  { name: "Backpack", price: 50 },
+]
+
+
+
+
+// Find()
+
+// Array of names
+const names = ["Alice", "Bob", "Charlie", "David", "Eva"];
+
+
+
+
+// Reduce()
+
+// Array of cart items
+const cart = [
+  { item: "T-shirt", price: 25 },
+  { item: "Jeans", price: 80 },
+  { item: "Sneakers", price: 60 },
+  { item: "Hat", price: 15 },
+  { item: "Backpack", price: 50 },
+];
+
+
+
+
+
+
+
+
+
+//Sort()
+// Array of students
+const students = [
+  { firstName: 'Susie', lastName: 'Hansen', grade: 76 },
+  { firstName: "Irma", lastName: "Norton", grade: 15 },
+  { firstName: "Baker", lastName: "Browning", grade: 69 },
+  { firstName: "Nolan", lastName: "Mclean", grade: 84 },
+  { firstName: "Hester", lastName: "Frye", grade: 95 }
+]
+
